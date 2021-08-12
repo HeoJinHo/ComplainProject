c46f6723ce4e907a6c58db70df924095cde0b5ee
jinho <hjh701234@gmail.com>
2021-08-12 14:52:35 +0900

    필요없는 소스 제거

--- src/main/java/com/complain/igex/rest/RestController.java
+++ src/main/java/com/complain/igex/rest/RestController.java
@@ -1,11 +1,14 @@
 package com.complain.igex.rest;
 
+import com.complain.igex.model.Member;
 import com.complain.igex.repository.MemberRepository;
+import com.complain.igex.searchData.MemberSearchData;
 import com.complain.igex.searchData.SeachData;
 import com.complain.igex.sv.ComplainSv;
 import com.complain.igex.sv.MemberSv;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Objects;
 
 @org.springframework.web.bind.annotation.RestController
@@ -23,27 +28,45 @@ public class RestController
 
     private final ComplainSv complainSv;
 
+    private final MemberSv memberSv;
+
+    private final MemberRepository memberRepository;
+
     @GetMapping("/test")
     public JSONObject tesr(
             @RequestHeader HttpHeaders httpHeaders,
             @PageableDefault(size = 10) Pageable pageable,
-            SeachData seachData){
+            SeachData seachData,
+            MemberSearchData memberSearchData){
         String tokens = "[e4cf459e-770b-45e3-a86b-76d04d9eae52]";
-        System.out.println(Objects.requireNonNull(httpHeaders.get("token")).toString());
 
-        if(tokens.equals(Objects.requireNonNull(httpHeaders.get("token")).toString()))
-            System.out.println("test");
+        HashMap<String, Object> map = new HashMap<>();
 
-        JSONObject json = new JSONObject();
+        if(tokens.equals(Objects.requireNonNull(httpHeaders.get("token")).toString())) {
+            map.put("member", memberSv.searchAll(pageable, memberSearchData));
+            map.put("result", complainSv.searchAll(pageable, seachData));
+        }else{
+            map.put("result", "TokenError");
+        }
+        return new JSONObject(map);
 
-        json.put("result", complainSv.searchAll(pageable, seachData));
-
-
-
-
-
-        return json;
 
     }
+
+    @GetMapping("/test1")
+    public JSONObject test22(String id, String name){
+        HashMap<String, Object> map = new HashMap<>();
+
+        memberRepository.findById(id);
+
+        map.put("result1", id);
+        map.put("result2", name);
+        map.put("result3", memberRepository.findById(id));
+        map.put("result4", "tt2tutti12");
+        map.put("result5", "tutti2tt12");
+
+        return new JSONObject(map);
+    }
+
 
 }
